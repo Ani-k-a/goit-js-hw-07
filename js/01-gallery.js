89df55a9afaad6e0104f5502598e08aeb398411b
Hanna Lytvynenko <ani-k-a@ukr.net>
2022-05-28 21:59:57 +0200

    gallery basic

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,20 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const gallery = document.querySelector(".gallery");
 
-console.log(galleryItems);
+function galleryItem() {
+    galleryItems.map(({ preview, original, description } ) => 
+        `<div class="gallery__item">
+        <a class="gallery__link" href="${original}">
+            <img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}"/>
+        </a>
+    </div>`).join("");
+};
+
+gallery.insertAdjacentHTML("afterbegin", galleryItem(galleryItems));
+
+// gallery.addEventListener('click', onModalOpen);
+
+    
+
+console.log(gallery);
