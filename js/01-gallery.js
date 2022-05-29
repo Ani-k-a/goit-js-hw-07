7d62df6cc428d59d4dcd292f4efeb0458173320b
Hanna Lytvynenko <ani-k-a@ukr.net>
2022-05-29 15:14:36 +0200

    hw 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,20 +1,43 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 const gallery = document.querySelector(".gallery");
+galleryItem();
 
 function galleryItem() {
-    galleryItems.map(({ preview, original, description } ) => 
-        `<div class="gallery__item">
-        <a class="gallery__link" href="${original}">
-            <img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}"/>
-        </a>
-    </div>`).join("");
+    const markup = galleryItems
+    .map(
+        ({ original, preview, description }) => 
+      `<div class="gallery__item">
+         <a class="gallery__link" href="${original}">
+             <img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}"/>
+          </a>
+       </div>`,)
+    .join('');
+    
+    gallery.innerHTML = markup;
 };
 
-gallery.insertAdjacentHTML("afterbegin", galleryItem(galleryItems));
+gallery.insertAdjacentHTML("beforeend", galleryItem(galleryItems));
 
-// gallery.addEventListener('click', onModalOpen);
 
-    
 
-console.log(gallery);
+
+let instance = null;
+galleryItem.addEventListener("click", ev => {
+    ev.preventDefault();
+    if (ev.target.nodeName !== "IMG") {
+        return;
+    }
+    instance = basicLightbox.create(
+        `<img src="${ev.target.dataset.source}" alt="${evt.target.alt}">`);
+    instance.show();
+    document.addEventListener('keydown', modalClose);
+});
+
+
+
+window.addEventListener('click', ev => {
+    if (ev.code === "Escape" && instance.visible()) {
+        instance.close();}
+    document.removeEventListener('keydown', modalClose);
+});
