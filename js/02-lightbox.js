441c3cb436ef85bb07deaaf7f3bc05ad4ecfdaa6
Hanna Lytvynenko <ani-k-a@ukr.net>
2022-05-31 16:10:07 +0200

    Fix 1+2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -6,19 +6,16 @@ function galleryItem() {
     const markup = galleryItems
     .map(
         ({ original, preview, description }) => 
-          `<a class="gallery__item" href="${original}">
+        `<div> 
+          <a class="gallery__item" href="${original}">
              <img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}"/>
-          </a>`)
+          </a>
+          </div>`)
     .join('');
     return markup;
 };
 
 gallery.insertAdjacentHTML("beforeend", galleryItem());
-
-gallery.addEventListener("click", (ev) => {
-  ev.preventDefault();
-});
-
 
 const lightbox = new SimpleLightbox('.gallery a', { 
   captionsData: "alt",
